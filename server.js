ad9055af321e20504faeb2103db308d3a39d5cd1
Zody <zodywoolsey@gmail.com>
2018-12-02 04:48:21 -0600

    Various additions/fixes
    Fixed linking issues between pages
    Addes eslint parser file
    Configured ESLint
    General cleanup of project as is
    _____________________________________________________________
    All previous are preperation for api implementation.

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 'use strict';
 const express = require('express'),
-    app = express();
+	app = express();
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/resources'));
 app.get('/', function(req,res){
-    res.render('login');
-})
+	res.render('login');
+});
 app.get('/home', function(req,res){
-    res.render('index');
-})
+	res.render('index');
+});
 app.get('/feed', function(req,res){
-    res.render('feed');
-})
+	res.render('feed');
+});
 
 app.listen(11000);
